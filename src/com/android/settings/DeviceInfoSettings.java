b99da01acf9ba931ce990f4eb1aa1786ad845d50
fusionjack <dogfight60-fusionjack@yahoo.de>
2013-12-12 19:11:11 +0100

    Added SlimSaber version in about phone
    Change-Id: I262fa50e7f2e913862a80fcae9899808a23fc9e5
    
    Conflicts:
    	res/values/slim_strings.xml
    	src/com/android/settings/DeviceInfoSettings.java

--- src/com/android/settings/DeviceInfoSettings.java
+++ src/com/android/settings/DeviceInfoSettings.java
@@ -69,6 +69,7 @@ public class DeviceInfoSettings extends SettingsPreferenceFragment {
     private static final String KEY_MOD_VERSION = "mod_version";
     private static final String KEY_DEVICE_CPU = "device_cpu";
     private static final String KEY_DEVICE_MEMORY = "device_memory";
+    private static final String KEY_SLIMSABER_VERSION = "slimsaber_version";
 
     static final int TAPS_TO_BE_A_DEVELOPER = 7;
 
@@ -92,6 +93,7 @@ public class DeviceInfoSettings extends SettingsPreferenceFragment {
         findPreference(KEY_BUILD_NUMBER).setEnabled(true);
         findPreference(KEY_KERNEL_VERSION).setSummary(getFormattedKernelVersion());
         setValueSummary(KEY_MOD_VERSION, "ro.slim.version");
+        setValueSummary(KEY_SLIMSABER_VERSION, "ro.modversion");
 
         if (!SELinux.isSELinuxEnabled()) {
             String status = getResources().getString(R.string.selinux_status_disabled);

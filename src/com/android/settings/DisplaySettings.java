8d921350d8e2e230b67f88ccd6a69a70b1a5f0ef
Danesh M <daneshm90@gmail.com>
2014-08-04 20:20:01 -0700

    Settings : Make proximity opt-in
      * Fix up code to compile
    
    Change-Id: Idfa64758a3c6d60ca4801f6f7f34f0429f95cbec
    Signed-off-by: Josue Rivera <prbassplayer@gmail.com>
    
    Conflicts:
    	src/com/android/settings/DisplaySettings.java

--- src/com/android/settings/DisplaySettings.java
+++ src/com/android/settings/DisplaySettings.java
@@ -110,6 +110,7 @@ public class DisplaySettings extends SettingsPreferenceFragment implements
     private CheckBoxPreference mSunlightEnhancement;
     private CheckBoxPreference mColorEnhancement;
     private PreferenceScreen mScreenColorSettings;
+    private CheckBoxPreference mProximityWake;
 
     private final Configuration mCurConfig = new Configuration();
 
@@ -207,6 +208,17 @@ public class DisplaySettings extends SettingsPreferenceFragment implements
                         Settings.System.VOLUME_WAKE_SCREEN, 0) == 1);
                 mVolumeWake.setOnPreferenceChangeListener(this);
             }
+        }
+
+        mProximityWake = (CheckBoxPreference) findPreference(KEY_PROXIMITY_WAKE);
+        if(!getResources().getBoolean(
+                com.android.internal.R.bool.config_proximityCheckOnWake)) {
+                mWakeUpOptions.removePreference(mProximityWake);
+                counter++;
+        } else {
+            mProximityWake.setChecked(Settings.System.getInt(getContentResolver(),
+                    Settings.System.PROXIMITY_ON_WAKE, 0) == 1);
+            mProximityWake.setOnPreferenceChangeListener(this);
         }
 
         mWakeUpWhenPluggedOrUnplugged =
@@ -596,7 +608,11 @@ public class DisplaySettings extends SettingsPreferenceFragment implements
                     Settings.System.WAKEUP_WHEN_PLUGGED_UNPLUGGED,
                     (Boolean) objValue ? 1 : 0);
         }
-
+        if (KEY_PROXIMITY_WAKE.equals(key)) {
+            Settings.System.putInt(getContentResolver(),
+                    Settings.System.PROXIMITY_ON_WAKE,
+                    ((Boolean) objValue).booleanValue() ? 1 : 0);
+        }
         return true;
     }
 

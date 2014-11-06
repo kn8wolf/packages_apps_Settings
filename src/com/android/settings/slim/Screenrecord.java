35bb9f13da2166119519d7d947fd20f8227bfd6e
kantjer <kantjer@gmail.com>
2014-11-06 12:28:18 +0100

    settings: Screen Recorder (3/3)
    Conflicts:
    	res/values-it/slim_strings.xml
    	res/values/slim_strings.xml
    
    Change-Id: Idf8295da0937b337f9b84d9a815779d133f9e92e

---
+++ src/com/android/settings/slim/Screenrecord.java
@@ -0,0 +1,79 @@
+/*
+ *  Copyright (C) 2013 The OmniROM Project
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+package com.android.settings.slim;
+
+import com.android.settings.SettingsPreferenceFragment;
+import com.android.settings.R;
+
+import android.content.ContentResolver;
+import android.content.Context;
+import android.os.Bundle;
+import android.preference.CheckBoxPreference;
+import android.preference.Preference;
+import android.preference.PreferenceGroup;
+import android.preference.PreferenceScreen;
+import android.provider.Settings;
+import android.provider.Settings.SettingNotFoundException;
+import android.util.Log;
+
+public class Screenrecord extends SettingsPreferenceFragment implements
+        Preference.OnPreferenceChangeListener {
+    private static final String TAG = "Screenrecord";
+    private ContentResolver resolver;
+
+    private static final String POWER_MENU_SCREENRECORD = "power_menu_screenrecord";
+
+    private CheckBoxPreference mScreenrecordPowerMenu;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.screenrecord_settings);
+
+        PreferenceScreen prefSet = getPreferenceScreen();
+        resolver = getActivity().getContentResolver();
+
+        boolean mHasScreenRecord = getActivity().getResources().getBoolean(
+                com.android.internal.R.bool.config_enableScreenrecordChord);
+
+        mScreenrecordPowerMenu = (CheckBoxPreference) prefSet.findPreference(POWER_MENU_SCREENRECORD);
+        if(mHasScreenRecord) {
+            mScreenrecordPowerMenu.setChecked(Settings.System.getInt(resolver,
+                    Settings.System.SCREENRECORD_IN_POWER_MENU, 0) == 1);
+            mScreenrecordPowerMenu.setOnPreferenceChangeListener(this);
+        } else {
+            prefSet.removePreference(mScreenrecordPowerMenu);
+        }
+    }
+
+    @Override
+    public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
+        return true;
+    }
+
+    public boolean onPreferenceChange(Preference preference, Object objValue) {
+        if (preference == mScreenrecordPowerMenu) {
+            boolean value = (Boolean) objValue;
+            Settings.System.putInt(resolver, Settings.System.SCREENRECORD_IN_POWER_MENU, value ? 1 : 0);
+        } else {
+            return false;
+        }
+        return true;
+    }
+}
